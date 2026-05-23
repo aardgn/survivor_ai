14ba0cc06cdacc1116c30261d07828db74204710
aardgn <gunarda205@gmail.com>
2026-05-23 16:25:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+GEMINI_KEY = os.getenv("GEMINI_API_KEY")
+GROQ_KEY = os.getenv("GROQ_API_KEY")
+
 HARITA_BOYUTU = 800
